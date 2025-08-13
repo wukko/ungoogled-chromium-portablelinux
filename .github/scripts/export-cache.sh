6e46f9020d73e3321457a9a1d120a5887589bdf9
jj <log@riseup.net>
2025-08-13 22:18:58 +0000

    ci: always use zstd
    the runner should always have it, let's not be
    safe when we control the environment

--- .github/scripts/export-cache.sh
+++ .github/scripts/export-cache.sh
@@ -2,17 +2,7 @@
 set -euxo pipefail
 
 _base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && cd ../.. && pwd)"
-
-_use_zstd=0
-if command -v zstd >/dev/null 2>&1; then
-    _use_zstd=1
-fi
-
-_ext="tar.xz"
-if [ "$_use_zstd" -eq 1 ]; then
-    _ext="tar.zst"
-fi
-_cache_tar="${_base_dir}/.github/cache/build-cache.${_ext}"
+_cache_tar="${_base_dir}/.github/cache/build-cache.tar.zst"
 
 pushd "${_base_dir}" >/dev/null
 
@@ -24,10 +14,6 @@ fi
 mkdir -p "$(dirname "${_cache_tar}")"
 
 # create archive via stream to avoid tar flag incompatibilities
-if [ "$_use_zstd" -eq 1 ]; then
-    tar -cf - "build" | zstd -f -T0 -3 -o "${_cache_tar}"
-else
-    tar -cf - "build" | xz -f -T0 -3 -c > "${_cache_tar}"
-fi
+tar -cf - "build" | zstd -f -T0 -3 -o "${_cache_tar}"
 
 popd >/dev/null
